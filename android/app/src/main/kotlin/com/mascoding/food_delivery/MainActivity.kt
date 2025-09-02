33bffd25d10f191d81d11a0bd9f020c62c85cc81
Mamadou-sar <megatif.sarr@gmail.com>
2025-09-02 14:54:08 +0200

    initial project

---
+++ android/app/src/main/kotlin/com/mascoding/food_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mascoding.food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  static const Color primary = Color(0xffff7622);
+  static const Color primaryVariant = Color(0xFFFFEBE4);
+  static const Color addCardBtn = Color(0xFFF58D1D);
+  static const Color red = Color(0xffE04444);
+  static const Color background = Color(0xFFFFFFFF);
+
+  // Additional colors
+  static const Color lightGray = Color(0xFFA0A5BA);
+  static const Color darkGray = Color(0xFF676767);
+  static const Color lightBlue = Color(0xFF98A8B8);
+  static const Color titleColor = Color(0xFF32343E);
+  static const Color categoryText = Color(0xFF181C2E);
+  static const Color textColor = Color(0xFF646982);
+  static const Color borderColor = Color(0xFFEDEDED);
+  static const Color inputBg = Color(0xFFF0F5FA);
+  static const Color authBg = Color(0xFF121223);
+  static const Color cardTitle = Color(0xFF32343E);
+}
