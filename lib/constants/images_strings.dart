bb054b0b8c5c5a74f51e8f4d2423862bd5379b2e
Mamadou-sar <megatif.sarr@gmail.com>
2025-09-02 15:03:28 +0200

    images setup

---
+++ lib/constants/images_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TImages {
+  static const String logo = 'assets/images/Logo.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String visa = 'assets/images/Visa.png';
+  static const String mastercard = 'assets/images/MasterCard.png';
+
+  // Onboarding Images
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+
+  // Social Media Icons
+  static const String google = 'assets/images/google.png';
+  static const String facebook = 'assets/images/facebook.png';
+  static const String twitter = 'assets/images/twitter.png';
+}
