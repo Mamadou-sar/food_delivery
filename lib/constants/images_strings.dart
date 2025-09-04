c72ff175f05929a450e571c2f002c716738ab882
Mamadou-sar <megatif.sarr@gmail.com>
2025-09-04 16:07:27 +0200

    sizes setup

--- lib/constants/images_strings.dart
+++ lib/constants/images_strings.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
-
 class TImages {
   static const String logo = 'assets/images/Logo.png';
   static const String profile = 'assets/images/profile.png';
   static const String visa = 'assets/images/Visa.png';
   static const String mastercard = 'assets/images/MasterCard.png';
+  static const String bg = 'assets/images/BG.png';
+  static const String splash = 'assets/images/Splash.png';
+  static const String trackOrder = 'assets/images/TrackOrder.png';
+  static const String group2644 = 'assets/images/Group2644.png';
+  static const String bgAsset = 'assets/images/BGAsset.png';
+  static const String card = 'assets/images/42.png';
 
   // Onboarding Images
   static const String onboarding1 = 'assets/images/onboarding1.png';
---
+++ lib/constants/sizes.dart
@@ -0,0 +1,33 @@
+class TSizes {
+  // Text Sizes
+  static const double smallTextSize = 10.0;
+  static const double interTextSize = 12.0;
+  static const double cardDescSize = 13.0;
+  static const double buttonTextSize = 14.0;
+  static const double cardTitleSize = 15.0;
+  static const double regularTextSize = 16.0;
+  static const double onboardTitleSize = 24.0;
+  static const double authSize = 30.0;
+
+  // Padding & Margin Sizes
+  static const double smallPadding = 5.0;
+  static const double interPadding = 10.0;
+  static const double regularPadding = 15.0;
+  static const double largePadding = 20.0;
+  static const double extraLargePadding = 30.0;
+
+  // Icon Sizes
+  static const double smallIconSize = 16.0;
+  static const double regularIconSize = 24.0;
+  static const double largeIconSize = 32.0;
+
+  // Border Radius Sizes
+  static const double smallBorderRadius = 5.0;
+  static const double regularBorderRadius = 10.0;
+  static const double buttonRadius = 12.0;
+  static const double imageCardRadius = 15.0;
+  static const double profileSectionRadius = 16.0;
+  static const double cardRadius = 24.0;
+  static const double largeBorderRadius = 30.0;
+  static const double categoryRadius = 33.0;
+}
